e69bb2800c7004a9566e132c5aa877db2d455859
Stephan Backhaus <stephan-backhaus@web.de>
2017-12-09 19:49:37 +0100

    Added day 6 part 1 + 2

---
+++ csharp_AdventOfCode2017/AdventOfCode2017/DaySix.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2017
+{
+    struct MaxBlockResult
+    {
+        public int Index { get; }
+        public int Value { get; }
+
+        public MaxBlockResult(int index, int value)
+        {
+            Index = index;
+            Value = value;
+        }
+    }
+
+    struct ComputationResult
+    {
+        public int CycleCount { get; }
+        public int LoopSize { get; }
+
+        public ComputationResult(int cycleCount, int loopSize)
+        {
+            CycleCount = cycleCount;
+            LoopSize = loopSize;
+        }
+    }
+
+    public class DaySix
+    {
+        private static string InputToCombo(List<int> list) => string.Join(", ", list);
+
+        private static MaxBlockResult FindMaxBlockBankIndexAndMax(List<int> list)
+        {
+            var max = list.Max();
+            return new MaxBlockResult(list.IndexOf(max), max);
+        }
+
+        private static int NextIndex(List<int> list, int currentIndex)
+        {
+            return currentIndex + 1 < list.Count
+                    ? currentIndex + 1
+                    : 0;
+        }
+
+        public static int ComputePartOne(List<int> inputList) => Compute(inputList).CycleCount;
+        public static int ComputePartTwo(List<int> inputList) => Compute(inputList).LoopSize;
+
+        private static ComputationResult Compute(List<int> inputList)
+        {
+            var stepCount = 0;
+            var combo = InputToCombo(inputList);
+            var uniqueCombinations = new List<string>(new[] { combo });
+            var comboStack = new List<string>(new[] { combo });
+
+            while (true)
+            {
+                stepCount++;
+                var workingBlock = FindMaxBlockBankIndexAndMax(inputList);
+                inputList[workingBlock.Index] = 0;
+                var index = NextIndex(inputList, workingBlock.Index);
+                for (var i = 0; i < workingBlock.Value; i++)
+                {
+                    inputList[index]++;
+                    index = NextIndex(inputList, index);
+                }
+                var currentCombo = InputToCombo(inputList);
+                if (uniqueCombinations.Contains(InputToCombo(inputList)))
+                {
+                    var lastSeen = comboStack.LastIndexOf(currentCombo);
+                    return new ComputationResult(stepCount, stepCount - lastSeen);
+                }
+
+                comboStack.Add(currentCombo);
+                uniqueCombinations.Add(currentCombo);
+            }
+        }        
+    }
+}
---
+++ csharp_AdventOfCode2017/AdventTests/06.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using AdventOfCode2017;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdventTests
+{
+    [TestClass]
+    public class _06
+    {
+        public static List<int> NewInputListInstance => new List<int>(new [] { 10, 3, 15, 10, 5, 15, 5, 15, 9, 2, 5, 8, 5, 2, 3, 6 });
+        public static List<int> NewExampleListInstance => new List<int>(new[] { 0, 2, 7, 0 });
+
+        [TestMethod]
+        public void Day6Part1Example()
+        {
+            Assert.AreEqual(5, DaySix.ComputePartOne(NewExampleListInstance));
+        }
+
+        [TestMethod]
+        public void Day6Part1()
+        {
+            Assert.AreEqual(14029, DaySix.ComputePartOne(NewInputListInstance));
+        }
+
+        [TestMethod]
+        public void Day6Part2Example()
+        {
+            Assert.AreEqual(4, DaySix.ComputePartTwo(NewExampleListInstance));
+        }
+
+        [TestMethod]
+        public void Day6Part2()
+        {
+            Assert.AreEqual(2765, DaySix.ComputePartTwo(NewInputListInstance));
+        }
+    }
+}
